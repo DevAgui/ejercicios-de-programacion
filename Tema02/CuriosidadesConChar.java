4fc76d87b5e0d999a2bdf2e9a209e14c9d0cbaf4
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-14 10:36:52 +0200

    Ejercicios tema 2

---
+++ Tema02/CuriosidadesConChar.java
@@ -0,0 +1,12 @@
+/**
+ * Uso de variables que contienen números decimales
+ *
+ * @author Luis J. Sánchez
+ */
+public class CuriosidadesConChar {
+  public static void main(String[] args) {
+  
+    String elTiempoDeHoy = "lluvioso"
+    System.out.println("Tiempo de hoy" + elTiempoDeHoy);
+  }
+}
---
+++ Tema02/Ejercicio01Tema02.java
@@ -0,0 +1,33 @@
+/**
+ * Uso de variables que contienen números decimales
+ *
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio01Tema02 {
+  public static void main(String[] args) {
+  
+    int x = 144;
+    
+    int y = 999;
+    
+    int suma = x + y;
+    
+    int resta = y - x;
+    
+    int division = y / x;
+    
+    int multiplicacion = x * y;
+    
+    System.out.println("El valor de x " + x);
+    
+    System.out.println("El valor de y " + y);
+    
+    System.out.println("El valor de la suma de x + y es " + suma);
+    
+    System.out.println("El valor de la resta de x - y es " + resta);
+    
+    System.out.println("El valor de la división de y / x es " + division);
+    
+    System.out.println("El valor de la multiplicación de x por y es " + multiplicacion);
+  }
+}
