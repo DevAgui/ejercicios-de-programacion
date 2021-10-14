157b286fc10b4a95d60acda6591d3aeda5e8f7db
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-14 10:56:28 +0200

    Ejercicios Tema 2, Variables.

---
+++ Tema02/Ejercicio02Tema02.java
@@ -0,0 +1,13 @@
+ /**
+ *Ejercicio 2 Tema Variables.
+ *Éste ejercicio nos pide crear una variable con nuestro nombre completo y que la mostremos en pantalla.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio02Tema02 {
+  public static void main(String[] args) {
+    
+    String nombre = "Diego Aguilera Martín";
+    
+    System.out.println("Mi nombre completo es " + nombre);
+  }
+}
