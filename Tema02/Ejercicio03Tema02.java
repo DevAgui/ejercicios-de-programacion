d9e401950ba92af1254ec29c5f10e691f71e6012
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-14 11:01:58 +0200

    Ejercicios del tema 2, Variables.

---
+++ Tema02/Ejercicio03Tema02.java
@@ -0,0 +1,19 @@
+ /**
+ *Ejercicio 3 Tema Variables.
+ *Éste ejercicio nos pide crear una variable con nuestro nombre completo, dirección y teléfono.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio03Tema02 {
+  public static void main(String[] args) {
+    
+    String nombre = "Diego Aguilera Martín.";
+    
+    String direccion= "Calle Falsa 123.";
+    
+    String telefono= "645343029.";
+    
+    System.out.println("Mi nombre completo es " + nombre);
+    System.out.println("Mi dirección es " + direccion);
+    System.out.println("Mi teléfono es " + telefono);
+  }
+}
