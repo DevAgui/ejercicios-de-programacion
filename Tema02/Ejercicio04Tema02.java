becf8a4a2f7d409e855e12f2c5b1c54f13c65e1d
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-14 20:33:49 +0200

    Conversor de euros a pesetas

--- Tema02/Ejercicio04Tema02.java
+++ Tema02/Ejercicio04Tema02.java
@@ -8,9 +8,9 @@ public class Ejercicio04Tema02 {
     
   double euros = 500d;
   
-  double conversion = euros / 166.386;
+  double conversion = euros * 166.386;
   
-  System.out.println("Tenemos esta cantidad de euros " + euros);
-  System.out.println("Que en pesetas sería " + conversion);
+  System.out.println("Tenemos esta cantidad de euros = " + euros);
+  System.out.println("Que en pesetas sería " + conversion + " pesetas");
   }
 }
