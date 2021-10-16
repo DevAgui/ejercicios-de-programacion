579dc0c6a537d7f299605f4f12b0bbcdaff9a3e5
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-16 12:37:38 +0200

    Ejercicios del Tema 2, Variables.

---
+++ Tema02/Ejercicio07Tema02.java
@@ -0,0 +1,21 @@
+ /**
+ *Ejercicio 7 Tema Variables.
+ *Éste ejercicio nos pide crear un programa que declare las variables tipo char y string en una sola línea. 
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio07Tema02 {
+  public static void main(String[] args) {
+  
+  char letraAleatoria1 = 'd';
+  
+  char letraAleatoria2 = 'j';
+  
+  char letraAleatoria3 = 'n';
+  
+  String fraseUno = "¿Qué hago haciendo esto en vez de estar de cañas?";
+  
+  String fraseDos = "En realidad puedo hacer esto mientras me tomo una cerveza";
+  
+  System.out.println(letraAleatoria1 + " " + fraseUno + " " + letraAleatoria2 + " " + fraseDos + " " + letraAleatoria3);
+  }
+}
