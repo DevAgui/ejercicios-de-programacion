a78f91c472348ee82774e562d16608cca84af0c0
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-16 13:11:06 +0200

    Ejercicios del Tema 2, Variables.

---
+++ Tema02/Ejercicio08Tema02.java
@@ -0,0 +1,24 @@
+ /**
+ *Ejercicio 8 Tema Variables.
+ *Éste ejercicio nos pide crear un programa que declare 5 variables tipo char, concatenándolas. 
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio08Tema02 {
+  public static void main(String[] args) {
+  
+  char letra1 = 'a';
+  
+  char letra2 = 'd';
+  
+  char letra3 = 'i';
+  
+  char letra4 = 'o';
+  
+  char letra5 = 's';
+  
+  String adios = "" + letra1 + letra2 + letra3 + letra4 + letra5 + "";
+  
+  System.out.println("Esto no es un " + adios + ", sino un hasta luego.");
+  
+  }
+}
