506c0d3df6bf4ad38495b772b7655ad502acf822
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-18 18:36:21 +0200

    Ejercicios del Tema 3

--- Tema03/Ejercicio01Tema03.java
+++ Tema03/Ejercicio01Tema03.java
@@ -6,9 +6,17 @@
 public class Ejercicio01Tema03 {
   public static void main(String[] args) {
   
-  System.out.println("Por favor, introduce el primer número: ");
+  System.out.println("Por favor, introduce un número: ");
   
-  int primerNumero = Integer.parseInt(System.console().readLine());
+  double primerNumero = Double.parseDouble(System.console().readLine());
+  
+  System.out.println("Por favor, introduce otro número: ");
+  
+  double segundoNumero = Double.parseDouble(System.console().readLine());
 
+  double multiplicacion = primerNumero * segundoNumero;
+  
+  System.out.printf("El resultado de multiplicar %3.2f y %3.2f es = %6.2f \n", primerNumero, segundoNumero, multiplicacion);
+  
   }
 }
