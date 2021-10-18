d0fbb875430e03bbc8f0d69cf6898e38e7e42cd6
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-18 10:15:18 +0200

    Ejercicios tema 3

---
+++ Tema03/Ejercicio01Tema03.java
@@ -0,0 +1,33 @@
+/**
+ * Ejercicio 1 Tema Variables.
+ *En éste ejercicio nos pide que mostremos los valores de x y de y, así como la suma, la resta, la división y la multiplicación.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio01Tema03 {
+  public static void main(String[] args) {
+  
+    int x = 144;
+    
+    int y = 999;
+    
+    int suma = x + y;
+    
+    int resta = y - x;
+    
+    int division = y / x;
+    
+    int multiplicacion = x * y;
+    
+    System.out.println("El valor de x " + x);
+    
+    System.out.println("El valor de y " + y);
+    
+    System.out.println("El valor de la suma de x + y es " + suma);
+    
+    System.out.println("El valor de la resta de x - y es " + resta);
+    
+    System.out.println("El valor de la división de y / x es " + division);
+    
+    System.out.println("El valor de la multiplicación de x por y es " + multiplicacion);
+  }
+}
