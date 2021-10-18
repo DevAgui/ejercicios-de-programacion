b1c299c164a0fcec0e80c5048ca5f266b66e38dd
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-18 19:24:56 +0200

    Ejercicios del Tema 3

---
+++ Tema03/Ejercicio05Tema03.java
@@ -0,0 +1,22 @@
+/**
+ * Ejercicio 5 Tema 3.
+ *Programa que nos pide calcular el área de un rectángulo.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio05Tema03 {
+  public static void main(String[] args) {
+  
+  
+  System.out.println("Por favor, introduce la base del rectángulo en cm: ");
+  
+  double base = Double.parseDouble(System.console().readLine());
+  
+  System.out.println("Por favor, introduce la altura del rectángulo en cm: ");
+  
+  double altura = Double.parseDouble(System.console().readLine());
+  
+  double bxh = base * altura;
+  
+  System.out.printf("El área de un rectángulo con b=%3.2f y h=%3.2f es igual a = %3.2f cm²", base, altura, bxh);  
+  }
+}
