37d6d0a343a9c75e44fb2b751792fbff8beaef78
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-19 10:52:25 +0200

    Ejercicios del Tema 3.

---
+++ Tema03/Ejercicio11Tema03.java
@@ -0,0 +1,15 @@
+ /**
+ *Ejercicio 11 Tema 3.
+ *Éste ejercicio nos pide crear un conversor de Kb a Mb. 
+ *@author Diego Aguilera Martín
+ */
+public class Ejercicio11Tema03 {
+  public static void main(String[] args) {
+    
+  System.out.println("Por favor, introduce los Kb que quieras pasar a Mb en números enteros: ");
+  int kb = Integer.parseInt(System.console().readLine());  
+  
+  int pasoMb = kb / 1000;
+  System.out.printf("%d Kb son %d Mb \n", kb, pasoMb);
+  }
+}
