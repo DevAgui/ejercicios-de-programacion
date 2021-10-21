e4d55ad0f02c8af1dd5cda395b3108e98a0a486f
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-21 11:16:17 +0200

    Ejercicios del Tema 4.

---
+++ Tema04/Ejercicio01Tema04.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 1 Tema 4.
+ * Éste ejercicio nos pide crear un programa capaz de calcular la nota que tienes que sacar en el segundo examen si quieres sacar x nota al final del trimestre. La primera nota vale el 40% del total, la segunda el 60%. 
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio01Tema04 {
+  public static void main(String[] args) {
+
+
+  }
+}
