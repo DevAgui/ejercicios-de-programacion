749f3cec028d6162c2d372058cd7b03cca2459d3
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-21 22:44:09 +0200

    Ejercicios del Tema 4.

---
+++ Tema04/Ejercicio05Tema04.java
@@ -0,0 +1,26 @@
+/**
+ * Ejercicio 5 Tema 4.
+ * Éste ejercicio nos pide crear un programa que resuelva ecuaciones de primer grado del tipo: ax+b=0. . 
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio05Tema04 {
+  public static void main(String[] args) {
+    
+    System.out.println("Éste programa te resuelve ecuaciones de primer grado del tipo: ax+b=0.");
+    System.out.println("");
+    System.out.println("Por favor, introduce a: ");
+    double a = Double.parseDouble(System.console().readLine()); 
+     
+    System.out.println("");
+    System.out.println("Por favor, introduce b: ");
+    double b = Double.parseDouble(System.console().readLine());
+    
+    System.out.println("");
+    double resolucionEcuacion = - b / a;
+    System.out.printf("x = %.3f", resolucionEcuacion );
+  
+        }
+    
+    
+  }
+
