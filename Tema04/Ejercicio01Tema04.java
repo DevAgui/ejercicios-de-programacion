4ea8176d7f2e7fc9671530bb03d67ca5f9b84588
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-21 19:19:36 +0200

    Ejercicios del Tema 4.

--- Tema04/Ejercicio01Tema04.java
+++ Tema04/Ejercicio01Tema04.java
@@ -1,11 +1,30 @@
 /**
  * Ejercicio 1 Tema 4.
- * Éste ejercicio nos pide crear un programa capaz de calcular la nota que tienes que sacar en el segundo examen si quieres sacar x nota al final del trimestre. La primera nota vale el 40% del total, la segunda el 60%. 
+ * Éste ejercicio nos pide crear un programa que pida por teclado un día de la semana y diga qué asignatura toca a primera hora.
  * @author Diego Aguilera Martín
  */
 public class Ejercicio01Tema04 {
   public static void main(String[] args) {
-
-gfgdfgdfg
+    
+    System.out.println("Éste programa te permite saber qué asignatura tienes el día que introduzcas por teclado.");
+    System.out.println("");
+    
+    System.out.println("Por favor, introduce un día de la semana, de lunes a viernes en minúsculas para saber qué asignatura tienes a primera hora: ");
+    String diaSemana = (System.console().readLine());
+    System.out.println("");
+    
+    if(diaSemana.equals("lunes")){
+      System.out.println("A primera hora tienes Programación.");
+    } else if (diaSemana.equals("martes")){
+      System.out.println("A primera hora tienes Programación.");
+      } else if (diaSemana.equals("miércoles" )){
+        System.out.println("A primera hora tienes Sistemas Informáticos.");
+        } else if(diaSemana.equals("jueves")){
+          System.out.println("A primera hora tienes Programación.");
+          } else if(diaSemana.equals("viernes")){
+            System.out.println("A primera hora tienes Sistemas Informáticos.");
+            } else{
+              System.out.println("¡Enhorabuena!¡No tienes clase :D!");
+              }
   }
 }
