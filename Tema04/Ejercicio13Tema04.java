a1f96788e91b6236b0a81f3836b81b4becced264
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-23 20:20:18 +0200

    Ejercicios del Tema 4.

--- Tema04/Ejercicio13Tema04.java
+++ Tema04/Ejercicio13Tema04.java
@@ -18,17 +18,17 @@ public class Ejercicio13Tema04 {
     int z = Integer.parseInt(System.console().readLine());
     
     if((x > y) && (x > z) && (y > z)){
-      System.out.println(x + y + z);
+      System.out.println(x + "," + y + "," + z+ ".");
       } else if((x > y) && (x > z) && (z > y)){
-          System.out.println(x + z + y);
+          System.out.println(x + "," + z + "," + y + ".");
         } else if((y > x) && (y > z) && (x > z)){
-            System.out.println(y + x + z);
+            System.out.println(y + "," + x + "," + z + ".");
           } else if((y > x) && (y > z) && (z > x)){
-              System.out.println(y + z + x);
+              System.out.println(y + "," + z + "," + x + ".");
             } else if((z > x) && (z > y) && (x > y)){
-                System.out.println(z + x + y);
+                System.out.println(z + "," + x + "," + y);
               } else if((z > x) && (z > y) && (y > x)){
-                  System.out.println(z + y + x);
+                  System.out.println(z + "," + y + "," + x);
                 }
 
         }
