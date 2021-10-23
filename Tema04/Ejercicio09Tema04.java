f1efba3991288478ba82829389b9af26de53e8c6
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-10-23 12:50:48 +0200

    Ejercicios del Tema 4.

--- Tema04/Ejercicio09Tema04.java
+++ Tema04/Ejercicio09Tema04.java
@@ -8,32 +8,37 @@ public class Ejercicio09Tema04 {
     
     System.out.println("Éste programa te resuelve ecuaciones de segundo grado del tipo: ax²+bx+c=0");
     System.out.println("");
+    
     System.out.println("Por favor, introduce a: ");
     double a = Double.parseDouble(System.console().readLine()); 
     System.out.println("");
-    
     System.out.println("Por favor, introduce b: ");
     double b = Double.parseDouble(System.console().readLine());
-    
+    System.out.println("");
     System.out.println("Por favor, introduce c: ");
     double c = Double.parseDouble(System.console().readLine());
+    System.out.println("");
     
+    double bPorB = b * b;
+    double aPorC = a * c;
+    double cuatroPorAporC = 4 * aPorC;
+    double bPorBmenosElResto = bPorB - cuatroPorAporC;
+    double raizCuadrada = Math.sqrt(bPorBmenosElResto);
+    double menosBsumando = -b + raizCuadrada;
+    double menosBrestando = -b - raizCuadrada;
+    double dosPorA = 2 * a;
+    double resolucionEcuacionPositiva = menosBsumando / dosPorA;
+    double resolucionEcuacionNegativa = menosBrestando / dosPorA;
+          
     if((b == 0) && (a == 0) && (c == 0)){
-      System.out.println("La ecuación no tiene solución real.");
+      System.out.println("La ecuación no tiene solución real. Introduce otros valores.");
       System.out.println("");
-      } else {
+      } else if(bPorBmenosElResto < 0){
+          System.out.println("La ecuación no tiene solución real. Introduce otros valores.");
           System.out.println("");
-          double bPorB = b * b;
-          double aPorC = a * c;
-          double cuatroPorAporC = 4 * aPorC;
-          double bPorDosMenosElResto = bPorB - cuatroPorAporC;
-          double raizCuadrada = Math.sqrt(bPorDosMenosElResto);
-          double menosBsumando = -b + raizCuadrada;
-          double menosBrestando = -b - raizCuadrada;
-          double dosPorA = 2 * a;
-          double resolucionEcuacionPositiva = menosBsumando / dosPorA;
-          double resolucionEcuacionNegativa = menosBrestando / dosPorA;
-          System.out.printf("x = %.6f y %.6f", resolucionEcuacionPositiva, resolucionEcuacionNegativa);
+         }else {
+            System.out.println("");
+            System.out.printf("x = %.6f y %.6f", resolucionEcuacionPositiva, resolucionEcuacionNegativa);
         }
     
 
