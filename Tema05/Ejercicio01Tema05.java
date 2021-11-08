2cdfe71900f61e002c5ebbd59ab66e21fe414e39
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-11-08 09:27:54 +0100

    Ejercicios del Tema 05-Bucles.

---
+++ Tema05/Ejercicio01Tema05.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 1 Tema 5.
+ * Éste ejercicio nos pide crear un programa que haga un bucle que muestre múltiplos de 5, de 0 a 100 usando for.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio01Tema05 {
+  public static void main(String[] args) {
+    
+    System.out.println("Bucle que muestre múltiplos de 5, de 0 a 100 usando for.");
+    System.out.println("");
+    
+    for (int x = 5; x <= 100; x += 5){
+    System.out.println(x);
+    System.out.println("");
+  }
+}
+}
