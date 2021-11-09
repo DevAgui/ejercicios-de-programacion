d54848e7ef3e3257893a2b29561bb5d8954dff0b
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-11-09 10:04:36 +0100

    Ejercicios del Tema05-Bucles.

---
+++ Tema05/Ejercicio05Tema05.java
@@ -0,0 +1,19 @@
+/**
+ * Ejercicio 5 Tema 5.
+ * Éste ejercicio nos pide crear un programa que haga un bucle que muestre los números del 320 al 160, contando de 20 en 20 usando un while.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio05Tema05 {
+  public static void main(String[] args) {
+    
+    System.out.println("Bucle que muestre los números del 320 al 160, contando de 20 en 20 usando while.");
+    System.out.println("");
+    int x = 320;
+    
+    while( x >= 160){
+    System.out.println(x);
+    System.out.println("");
+    x -= 20;
+  }
+}
+}
