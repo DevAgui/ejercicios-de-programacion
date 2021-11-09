2cd77e78cc54252f1fbed3b8556ea7102baa76c3
Diego Aguilera Martín <dagmartin98@gmail.com>
2021-11-09 09:53:45 +0100

    Ejercicios del Tema05-Bucles.

---
+++ Tema05/Ejercicio02Tema05.java
@@ -0,0 +1,19 @@
+/**
+ * Ejercicio 2 Tema 5.
+ * Éste ejercicio nos pide crear un programa que haga un bucle que muestre múltiplos de 5, de 0 a 100 usando while.
+ * @author Diego Aguilera Martín
+ */
+public class Ejercicio02Tema05 {
+  public static void main(String[] args) {
+    
+    System.out.println("Bucle que muestre múltiplos de 5, de 0 a 100 usando while");
+    System.out.println("");
+    
+    int x = 0;
+    while(x <= 100){
+    System.out.println(x);
+    x += 5;
+    System.out.println("");
+  }
+}
+}
