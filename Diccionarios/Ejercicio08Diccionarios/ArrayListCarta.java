17ce734c37bbd25779b999f74f34fe370371d9a6
Diego Aguilera Martín <91873599+DiegoAguileraMartin@users.noreply.github.com>
2022-03-22 11:24:22 +0100

    Add files via upload
    Actualizados los ejercicios de Cartas de la Baraja Española

--- Diccionarios/Ejercicio08Diccionarios/ArrayListCarta.java
+++ Diccionarios/Ejercicio08Diccionarios/ArrayListCarta.java
@@ -9,70 +9,24 @@ almacenarlas y asegúrate de que no se repite ninguna. */
 public class ArrayListCarta{
     public static void main(String[] args) {
     
-        // Declaración de ArrayList
+        // Declaración de ArrayList barajaEspanola
         ArrayList<Carta> barajaEspanola = new ArrayList<Carta>();
 
-
-        // Creación las cartas de la baraja española
-
-        //Bastos
-        barajaEspanola.add(new Carta("AS", "Bastos"));
-        barajaEspanola.add(new Carta("2", "Bastos"));
-        barajaEspanola.add(new Carta("3", "Bastos"));
-        barajaEspanola.add(new Carta("4", "Bastos"));
-        barajaEspanola.add(new Carta("5", "Bastos"));
-        barajaEspanola.add(new Carta("6", "Bastos"));
-        barajaEspanola.add(new Carta("7", "Bastos"));
-        barajaEspanola.add(new Carta("Sota", "Bastos"));
-        barajaEspanola.add(new Carta("Caballo", "Bastos"));
-        barajaEspanola.add(new Carta("Rey", "Bastos"));
-
-        // Copas
-        barajaEspanola.add(new Carta("AS", "Copas"));
-        barajaEspanola.add(new Carta("2", "Copas"));
-        barajaEspanola.add(new Carta("3", "Copas"));
-        barajaEspanola.add(new Carta("4", "Copas"));
-        barajaEspanola.add(new Carta("5", "Copas"));
-        barajaEspanola.add(new Carta("6", "Copas"));
-        barajaEspanola.add(new Carta("7", "Copas"));
-        barajaEspanola.add(new Carta("Sota", "Copas"));
-        barajaEspanola.add(new Carta("Caballo", "Copas"));
-        barajaEspanola.add(new Carta("Rey", "Copas"));
-
-        // Espadas
-        barajaEspanola.add(new Carta("AS", "Espadas"));
-        barajaEspanola.add(new Carta("2", "Espadas"));
-        barajaEspanola.add(new Carta("3", "Espadas"));
-        barajaEspanola.add(new Carta("4", "Espadas"));
-        barajaEspanola.add(new Carta("5", "Espadas"));
-        barajaEspanola.add(new Carta("6", "Espadas"));
-        barajaEspanola.add(new Carta("7", "Espadas"));
-        barajaEspanola.add(new Carta("Sota", "Espadas"));
-        barajaEspanola.add(new Carta("Caballo", "Espadas"));
-        barajaEspanola.add(new Carta("Rey", "Espadas"));
-
-        // Oros
-        barajaEspanola.add(new Carta("AS", "Oros"));
-        barajaEspanola.add(new Carta("2", "Oros"));
-        barajaEspanola.add(new Carta("3", "Oros"));
-        barajaEspanola.add(new Carta("4", "Oros"));
-        barajaEspanola.add(new Carta("5", "Oros"));
-        barajaEspanola.add(new Carta("6", "Oros"));
-        barajaEspanola.add(new Carta("7", "Oros"));
-        barajaEspanola.add(new Carta("Sota", "Oros"));
-        barajaEspanola.add(new Carta("Caballo", "Oros"));
-        barajaEspanola.add(new Carta("Rey", "Oros"));
-
-
         // Selección aleatoria de 10 cartas de la baraja
+
+        String [] figuras = {"AS","2","3","4","5","6","7","Sota","Caballo","Rey"};
+        
+        String [] palos = {"Bastos","Copas","Espadas","Oros"};
 
         int seleccionAleatoria = (int)(Math.random() * 10);
 
         
-        for(int i = 0; i < 9; i++){
+        for (String figura: figuras) {
 
-            System.out.println(barajaEspanola);
-        }
+            barajaEspanola.add(new Carta(figura, palos));
+
+        } 
+    
 
 
 
