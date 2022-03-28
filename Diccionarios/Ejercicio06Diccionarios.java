e20a904e3155f178486f9f59bfdec18fceab644b
Diego Aguilera Martín <91873599+DiegoAguileraMartin@users.noreply.github.com>
2022-03-28 10:07:04 +0200

    Add files via upload

--- Diccionarios/Ejercicio06Diccionarios.java
+++ Diccionarios/Ejercicio06Diccionarios.java
@@ -11,64 +11,65 @@ de usuario con sus correspondientes contraseñas deben estar almacenados en
 una estructura de la clase HashMap. */
 
 public class Ejercicio06Diccionarios {
-    
+
     public static void main(String[] args) {
-        
 
         // Declaración de Scanner
         Scanner s = new Scanner(System.in);
 
         // Introducción de datos en el HashMap 'usuarios'
-        HashMap<Integer, String> usuarios = new HashMap<Integer, String>();
+        HashMap<String, Integer> usuarios = new HashMap<String, Integer>();
 
-        usuarios.put(1234, "Diego");
-        usuarios.put(0101, "Alvaro");
-        usuarios.put(9999, "Fran");
-        usuarios.put(8661, "David");
-
+        usuarios.put("Diego", 1234);
+        usuarios.put("Alvaro", 0101);
+        usuarios.put("Fran", 7583);
+        usuarios.put("David", 8661);
+        usuarios.put("Antonio", 9999);
 
         // Declaración de variables para el control de acceso
         int oportunidades = 3;
         boolean conexionExitosa = true;
 
-
         // Control de acceso
-        do{
+        do {
 
-            System.out.println("\nPor favor, introduzca su nombre completo");
+            System.out.println("\nPor favor, introduzca su nombre de usuario: ");
             String nombre = s.nextLine();
 
             System.out.println("A continuación introduzca su contraseña: ");
             Integer contrasena = Integer.parseInt(s.nextLine());
 
-            if((usuarios.containsKey(contrasena) && usuarios.containsValue(nombre))){
-
-                System.out.println("Ha accedido al área restringida");
-                oportunidades = 0;
-                
-
-            } else{
-
             
+            if ((usuarios.containsKey(nombre))) {
+
+                if (contrasena.equals(usuarios.get(nombre))) {
+
+                    System.out.println("Ha accedido al área restringida");
+                    oportunidades = 0;
+
+                } else {
+
+                    System.out.print("Se ha equivocado al introducir la contraseña.\n");
+
+                    oportunidades--;
+                }
+            } else {
+
                 oportunidades--;
 
-                System.out.print("\nSe ha equivocado al introducir el usuario ");
-                System.out.print("y/o la contraseña. Le quedan " + oportunidades);
-                System.out.print(" oportunidades.\n\n");
+                System.out.print("\nSe ha equivocado al introducir el usuario.");
 
                 conexionExitosa = false;
 
-                if(oportunidades == 0){
+                if (oportunidades == 0) {
 
                     System.out.println("\nLo siento, no tiene acceso al área restringida");
-        
+
                 }
-    
 
             }
 
-            
-        }while(oportunidades > 0);
+        } while (oportunidades > 0);
 
     }
 }
